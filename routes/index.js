3e49d4d2c2dfe40b469f794c44f2c40781b623be
ombak <coklatpanas83@gmail.com>
2023-03-08 22:54:54 +0700

    clear caches

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.status(200).json({
+        "status": "success",
+        "message": "Selamat datang di RESTful APIs data wilayah di Indonesia",
+        "license": "https://mit-license.org/",
+        "author": "ombak"
+    });
 });
 
 module.exports = router;
